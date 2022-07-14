85dca144e4a23fe3ba100832f3996e985a79e6e1
Kushagra Sharma <98047633+2115500082@users.noreply.github.com>
2022-07-14 17:34:47 +0530

    Delete Diamond.py

--- Diamond.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter the number of rows: "))  
-k = 0 
-for i in range(n, -1, -1):  
-    for j in range(k, 0, -1):  
-        print(end=" ")  
-    k = k + 1  
-    for j in range(0, i + 1):  
-        print("*", end=" ")  
-    print("")  
